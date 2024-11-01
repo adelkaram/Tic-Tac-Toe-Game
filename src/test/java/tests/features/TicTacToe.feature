e7ac94f00a5310f1dc980d4e9c6f3c3bb27877d0
adelkaram55 <adelkaram0001@gmail.com>
2024-11-01 14:35:54 +0200

    the first test case

--- src/test/java/tests/features/TicTacToe.feature
+++ src/test/java/tests/features/TicTacToe.feature
@@ -3,4 +3,7 @@ Feature: Tic Tac Toe Game
   Scenario: Player X wins the game
     Given the Tic Tac Toe game is open
     When player X selects cell 0
+    Then cell 0 should display "X"
+    When player O attempts to select cell 0
+    Then cell 0 should still display "X" and should not be overridden
 
--- src/test/java/tests/features/TicTacToeSteps.java
+++ src/test/java/tests/features/TicTacToeSteps.java
@@ -1,6 +1,7 @@
 package tests.features;
 
 import io.cucumber.java.en.*;
+import org.junit.Assert;
 import tests.pages.TicTacToePage;
 import tests.BaseTest;
 
@@ -19,6 +20,21 @@ public class TicTacToeSteps extends BaseTest {
     @When("player X selects cell {int}")
     public void playerXSelectsCell(int cellNumber) {
         ticTacToePage.selectCell(cellNumber);
+    }
+
+    @Then("cell {int} should display {string}")
+    public void cellShouldDisplay(int cellNumber, String expectedValue) {
+        Assert.assertEquals(ticTacToePage.getCell(cellNumber).getText(), expectedValue);
+    }
+
+    @When("player O attempts to select cell {int}")
+    public void playerOAttemptsToSelectCell(int cellNumber) {
+        ticTacToePage.selectCell(cellNumber);
+    }
+
+    @Then("cell {int} should still display {string} and should not be overridden")
+    public void cellShouldNotBeOverridden(int cellNumber, String expectedValue) {
+        Assert.assertEquals(ticTacToePage.getCell(cellNumber).getText(), expectedValue);
     }
 
 
